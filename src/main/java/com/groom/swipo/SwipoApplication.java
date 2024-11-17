d68eb0c6faabbf44977b648663cd9d18fda6c219
Junho Yu <95111999+ryuwon2407@users.noreply.github.com>
2024-11-18 00:24:35 +0900

    ✨ Feat: 프로젝트 초기파일

---
+++ src/main/java/com/groom/swipo/SwipoApplication.java
@@ -0,0 +1,13 @@
+package com.groom.swipo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwipoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwipoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groom/swipo/SwipoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groom.swipo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwipoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

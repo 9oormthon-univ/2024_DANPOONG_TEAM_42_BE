534249d8204e0ab679bf434476b146f1af4881fa
Kang Duoh <kduoh99@gmail.com>
2024-11-18 05:49:15 +0900

    ✨Feat[#3] JWT 로직 구현

---
+++ src/main/java/com/groom/swipo/global/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.groom.swipo.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.groom.swipo.global.jwt.JwtFilter;
+import com.groom.swipo.global.jwt.TokenProvider;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private final TokenProvider tokenprovider;
+	private final String[] PERMIT_ALL_URLS = {
+		"/v1/**"
+	};
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+		return httpSecurity
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.csrf(AbstractHttpConfigurer::disable)
+			.sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(
+				SessionCreationPolicy.STATELESS))
+			.formLogin(AbstractHttpConfigurer::disable)
+			.logout(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests(authorizeRequests -> authorizeRequests
+				.requestMatchers(PERMIT_ALL_URLS).permitAll()
+				.anyRequest().authenticated()
+			)
+			.addFilterBefore(new JwtFilter(tokenprovider), UsernamePasswordAuthenticationFilter.class)
+			.build();
+	}
+}

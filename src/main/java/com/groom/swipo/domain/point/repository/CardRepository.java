ac1cbd358f255554cc121ddda238d2441966f7bd
Junho Yu <95111999+ryuwon2407@users.noreply.github.com>
2024-11-22 22:04:38 +0900

    ✨ Feat[#25]: 스윕페이 홈 API를 위한 Repository 설정 - 2

--- src/main/java/com/groom/swipo/domain/point/repository/CardRepository.java
+++ src/main/java/com/groom/swipo/domain/point/repository/CardRepository.java
@@ -1,10 +1,14 @@
 package com.groom.swipo.domain.point.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.groom.swipo.domain.point.entity.Card;
+import com.groom.swipo.domain.user.entity.User;
 
 @Repository
 public interface CardRepository extends JpaRepository<Card, Long> {
+	List<Card> findAllByUser(User user);
 }

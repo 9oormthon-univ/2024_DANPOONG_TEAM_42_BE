d0588a5a26b3c83520f00429a7dd1595571eb0c0
Junho Yu <95111999+ryuwon2407@users.noreply.github.com>
2024-11-23 00:59:52 +0900

    ✨ Feat[#25]: 카드 유무 여부 확인 코드 추가

--- src/main/java/com/groom/swipo/domain/point/repository/CardRepository.java
+++ src/main/java/com/groom/swipo/domain/point/repository/CardRepository.java
@@ -7,8 +7,10 @@ import org.springframework.stereotype.Repository;
 
 import com.groom.swipo.domain.point.entity.Card;
 import com.groom.swipo.domain.user.entity.User;
+import com.groom.swipo.global.common.enums.Area;
 
 @Repository
 public interface CardRepository extends JpaRepository<Card, Long> {
 	List<Card> findAllByUser(User user);
+	boolean existsByUserAndArea(User user, Area area);
 }

92328050525992a21a2731c4d62d038989643958
Junho Yu <95111999+ryuwon2407@users.noreply.github.com>
2024-11-22 17:08:07 +0900

    ✨ Feat[#21]: 스윔스톤 교환 Dto

---
+++ src/main/java/com/groom/swipo/domain/point/dto/Request/SwipstoneSwapRequest.java
@@ -0,0 +1,10 @@
+package com.groom.swipo.domain.point.dto.Request;
+
+import java.util.List;
+
+public record SwipstoneSwapRequest(
+	Integer point,
+	Integer usePieceNum,
+	List<Long> myPieceIds
+) {}
+
